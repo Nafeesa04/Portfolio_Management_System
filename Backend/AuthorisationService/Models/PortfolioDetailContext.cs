9929d833fdec67514e0666c847b262980d2b419a
Nafeesa04 <109400456+Nafeesa04@users.noreply.github.com>
2022-07-16 12:18:29 +0530

    Add files via upload

---
+++ Backend/AuthorisationService/Models/PortfolioDetailContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Authorization.Models
+{
+    public class PortfolioDetailContext:DbContext
+    {
+        public PortfolioDetailContext(DbContextOptions<PortfolioDetailContext>options):base(options)
+        {
+
+        }
+        public DbSet<PortfolioDetail> PortfolioDetails { get; set; }
+    }
+}
